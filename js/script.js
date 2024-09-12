cf0a66dec296b2d6ed6573ad766ed938008ff76d
Helder <162512094+Casthely@users.noreply.github.com>
2024-09-12 00:46:40 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -30,15 +30,16 @@ $(document).ready(function () {
     new Typed('#type-it', {
         strings: ['Easy!!', 'Digital!'],
         typeSpeed: 200,
+        backDelay: 1000,
         loop: true,
         fadeOut: true,
         fadeOutClass: 'typed-fade-out',
         fadeOutDelay: 500,
     });
     $.scrollIt({
-        easing: 'linear',      // the easing function for animation
-        topOffset: -70,           // offste (in px) for fixed top navigation
-      });
+        easing: 'linear',
+        topOffset: -70,
+    });
 });
 
 
@@ -100,3 +101,46 @@ emailForm.addEventListener("submit", function (event) {
         }, 5000)
     }
 });
+
+/*======================================= End / Validar e-mail =======================================*/
+/*======================================= Scroll Section =======================================*/
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offste = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offste && top < offste + height) {
+            navLinks.forEach.apply(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
+            });
+        };
+
+    });
+};
+
+/*======================================= End / Scroll Section =======================================*/
+/*======================================= Scroll Reveal =======================================*/
+
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-intro h6, .home-intro', { origin: 'left' });
+ScrollReveal().reveal('.home-image', { origin: 'right' });
+ScrollReveal().reveal('.about-img', { origin: 'left' });
+ScrollReveal().reveal('.about-text', { origin: 'right' });
+ScrollReveal().reveal('.service-box', { origin: 'top' });
+ScrollReveal().reveal('.overview', { origin: 'left' });
+ScrollReveal().reveal('.maria-serva', { origin: 'left' });
+ScrollReveal().reveal('.jaavm', { origin: 'right' });
+ScrollReveal().reveal('.sao-bras', { origin: 'right' });
+ScrollReveal().reveal('.tes-gestao', { origin: 'left' });
